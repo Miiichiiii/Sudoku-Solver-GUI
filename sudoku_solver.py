43b0de9f7dc6f046aa0004a4664d1dfedfb834b4
Miiichiiii <bue.mich38@gmail.com>
2022-11-06 10:30:25 +0100

    Added comments

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -6,21 +6,27 @@ class Sudoku:
         self.sudoku = deepcopy(l)
 
     def getBlock(self, x, y):
+        # Return the block that contains the cell with coordinates x and y
         return [u for i in self.sudoku[y * 3:y * 3 + 3] for u in i[x * 3:x * 3 + 3]]
 
     def getRow(self, y):
+        # Return the current row
         return self.sudoku[y]
 
     def getColumn(self, x):
+        # Return the current column
         return [y[x] for y in self.sudoku]
 
     def getNotDefined(self):
+        # Return a list with coordinates, which points to empty cells
         return [(x, y) for y, e in enumerate(self.sudoku) for x, _ in enumerate(e) if not self.getElem(x, y)]
 
     def getElem(self, x, y):
+        # Return the current value of the cell
         return self.sudoku[y][x]
 
     def getPossibilities(self, x, y):
+        # Return all the numbers that are possible for the current cell
         f = set()
         for y0, x0, b0 in zip(self.getRow(y), self.getColumn(x), self.getBlock(x // 3, y // 3)):
             f.add(y0)
@@ -31,9 +37,12 @@ class Sudoku:
         return [x for x in range(1, 10) if x not in f]
 
     def write(self, x, y, item):
+        # Write the item to the cell
         self.sudoku[y][x] = item
 
     def get_first_not_defined(self):
+        # Search in the Sudoku for the first element which has no value
+        # Probally inefficient TODO
         for y in range(9):
             for x in range(9):
                 if not self.sudoku[y][x]:
@@ -53,6 +62,7 @@ class Sudoku:
         return False
 
     def validate(self):
+        # Returns true if the sudoku is valid, false otherwise
         rows = [set() for _ in range(9)]
         columns = [set() for _ in range(9)]
         blocks = [[set() for _ in range(3)] for _ in range(3)]
@@ -84,6 +94,7 @@ class Sudoku:
                     changed = True
 
     def is_solved(self):
+        # Return true if the sudoku is solved
         return True if not self.getNotDefined() else False
 
     def solve(self):
